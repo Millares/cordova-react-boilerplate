c22c9164fe35ec71257f4e7b106f2e2c8d0e9181
Lazaro Edreniel Millares Bermudez <lazaro.millares@tutanota.com>
2018-08-13 00:36:40 -0400

    Initial commit

---
+++ src/containers/app.container.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return <h1>PROBANDO</h1>;
+  }
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './containers/app.container';
+
+document.addEventListener('deviceready', function () {
+  ReactDOM.render(
+    <App>
+    </App>,
+    document.getElementById('app')
+  );
+}, false);
